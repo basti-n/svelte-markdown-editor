69638b043b57939091086c3f413f8630a7fd0390
Sebastian Neumair <sebastianneumair@Sebastians-MBP.fritz.box>
2020-03-22 08:15:48 +0100

    Add SCSS setup

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import marked from "marked";
+
+  let source = "# new Document";
+  let markdown = marked(source);
+</script>
+
+<style lang="scss">
+  .header {
+    .header-title {
+      color: lavender;
+    }
+  }
+</style>
+
+<main>
+  <header class="header">
+    <h1 class="header-title">Markdown Editor (made with Svelte)</h1>
+  </header>
+
+  <p>{source}</p>
+  <p>{markdown}</p>
+</main>
